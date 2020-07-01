8c243ebfe92ae5876f927fd4562f0d5ac76ccf0d
Maria Mihai <maria.ic.mihai@gmail.com>
2020-07-01 14:32:52 +0200

    Sending and receiving a synchronous message. Closes #9

--- src/main/java/guru/springframework/sbmjms/config/JmsConfig.java
+++ src/main/java/guru/springframework/sbmjms/config/JmsConfig.java
@@ -10,6 +10,7 @@ import org.springframework.jms.support.converter.MessageType;
 public class JmsConfig {
 
     public static final String MY_QUEUE = "hello-world";
+    public static final String MY_SEND_RCV_QUEUE = "reply-back";
 
     @Bean
     public MessageConverter messageConverter() {
